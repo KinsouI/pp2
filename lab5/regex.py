86633fee848dad96f41fe733cfc3548ee0e6d88a
Eldar <patt0n555@mail.ru>
2025-03-05 07:08:17 +0600

    another part

--- lab5/regex.py
+++ lab5/regex.py
@@ -13,3 +13,26 @@ pattern5 = r"a.+b"
 pattern6 = r"( |,|\.)"
 def task6(text):
   return re.sub(pattern6, ";", text)
+
+
+
+pattern7 = r"[a-z]_[a-zA-Z]"
+def task7(text):
+  return re.sub(
+    pattern7,
+    lambda match: match.group(0)[0] + match.group(0)[2].upper(),
+    text
+  )
+
+
+pattern8 = r"[A-Z]"
+def task8(text):
+  return re.sub(
+    pattern8,
+    lambda match: (" " if match.start() > 0 else "") + (match.group(0)[0]) + (" " if match.end() < len(text) else ""),
+    text
+  )
+  return re.split(
+    pattern8,
+    text
+  )
